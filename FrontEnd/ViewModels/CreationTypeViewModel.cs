8850760d5e0564495417d4e496c83d34f14c5a2e
TheGreatLemoncode <yaopascaljoseph@gmail.com>
2026-03-31 03:21:15 -0400

    avancement classe

---
+++ FrontEnd/ViewModels/CreationTypeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEnd.ViewModels
+{
+    internal class CreationTypeViewModel
+    {
+        public byte Value;
+    }
+}
